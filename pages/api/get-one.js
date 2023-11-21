e8520ab0ecb23fe8487c656ff6e4e2835f562a68
marco0antonio0 <marcomesquitajr@hotmail.com>
2023-11-20 22:30:13 -0300

    up files

--- pages/api/get-one.js
+++ pages/api/get-one.js
@@ -1,5 +1,5 @@
-import { pokemons_db } from "../../db/dados";
-export default function handler(req, res) {
+import DatabaseConnection from "../../connection/connectio";
+export default async function handler(req, res) {
   //============================================================
   // cors aplicado
   res.setHeader("Access-Control-Allow-Credentials", true);
@@ -13,20 +13,51 @@ export default function handler(req, res) {
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
   );
   //============================================================
+  // solicita o parametro ID na url chamada da api
   const { id } = req.query;
+
   if (id) {
-    if (id < pokemons_db.length) {
-      res.status(200).json({
-        message: "Esta é uma rota de API de get-one",
-        status: true,
-        data: pokemons_db[id],
-      });
-    } else {
-      res.status(400).json({
-        message: "id solicitado fora do range da lista",
+    //============================================================
+    // import database conexão
+    const db = new DatabaseConnection();
+    //============================================================
+    try {
+      // faz a conexão com a instacia
+      await db.connect();
+      // query a ser executada com a clausula WHERE indicando uma pesquisa de acordo com o >>ID<<
+      const sql = `SELECT * FROM Pokemon WHERE id = ${id}`; // query SQL atribuida a variavel
+      // query a sendo executada e armazenada o resultado
+      const results = await db.query(sql); // query SQL sendo executada
+      // ==================================================
+      // condicional executa caso tenha retorno da query
+      if (results) {
+        // entrega da mensagem na chamada
+        res.status(200).json({
+          message: "Esta é uma rota de API get-one",
+          status: true,
+          data: results,
+        });
+      } else {
+        // ==================================================
+        // condicional executa caso NÃO tenha retorno da query
+        // entrega da mensagem de erro na chamada
+        res.status(404).json({
+          message: "não informado ou inexistente",
+          status: false,
+          data: results,
+        });
+      }
+    } catch (error) {
+      // ==================================================
+      // entrega da mensagem de erro na chamada
+      res.status(500).json({
+        message: "Erro interno do servidor",
         status: false,
         data: [],
       });
+    } finally {
+      // Certifique-se de fechar a conexão quando terminar
+      await db.close(); // por fim ele encerra a conexão
     }
   } else {
     res.status(400).json({
--- pages/api/sort.js
+++ pages/api/sort.js
@@ -1,10 +1,5 @@
-import { pokemons_db } from "../../db/dados";
-
-function gerarNumeroInteiroAleatorio(min, max) {
-  // Use Math.floor para arredondar para baixo o resultado
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-export default function handler(req, res) {
+import DatabaseConnection from "../../connection/connectio";
+export default async function handler(req, res) {
   //============================================================
   // cors aplicado
   res.setHeader("Access-Control-Allow-Credentials", true);
@@ -18,12 +13,47 @@ export default function handler(req, res) {
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
   );
 
-  var numeroAleatorio = gerarNumeroInteiroAleatorio(0, pokemons_db.length);
-
   //============================================================
-  res.status(200).json({
-    message: "Esta é uma rota de API de sorteio de pokemon",
-    status: true,
-    data: pokemons_db[numeroAleatorio],
-  });
+  // import database conexão
+  const db = new DatabaseConnection();
+  //============================================================
+  try {
+    // faz a conexão com a instacia
+    await db.connect();
+
+    // query a ser executada com a clausula ORDER BY RAND() que indica um sorteio de pokemon
+    const sql = `SELECT * FROM Pokemon ORDER BY RAND() LIMIT 1;`; // query SQL atribuida a variavel
+    // query a sendo executada e armazenada o resultado
+    const results = await db.query(sql); // query SQL sendo executada
+    // ==================================================
+    // condicional executa caso tenha retorno da query
+    if (results) {
+      // entrega da mensagem na chamada
+      res.status(200).json({
+        message: "Esta é uma rota de API sort",
+        status: true,
+        data: results,
+      });
+    } else {
+      // ==================================================
+      // condicional executa caso NÃO tenha retorno da query
+      // entrega da mensagem de erro na chamada
+      res.status(404).json({
+        message: "não informado ou inexistente",
+        status: false,
+        data: results,
+      });
+    }
+  } catch (error) {
+    // ==================================================
+    // entrega da mensagem de erro na chamada
+    res.status(500).json({
+      message: "Erro interno do servidor",
+      status: false,
+      data: [],
+    });
+  } finally {
+    // Certifique-se de fechar a conexão quando terminar
+    await db.close(); // por fim ele encerra a conexão
+  }
 }
